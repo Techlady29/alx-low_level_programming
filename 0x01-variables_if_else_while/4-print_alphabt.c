a90d1f152835380d43254a69b393bf43033d765b
Techlady29 <triumphnwabuko29@gmail.com>
2023-03-18 06:37:01 -0700

    Even more alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - to Write a program that prints the alphabet in lowercase
+ * followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+	if (ch != 'q' && ch != 'e')
+}
+{
+putchar(ch);
+}
+putchar(10); /* new line code*/
+return (0);
+
+}
