9d62ae3fb2558e96e82917382775b42ad93281bc
Techlady29 <triumphnwabuko29@gmail.com>
2023-03-22 04:43:53 -0700

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,5 +8,15 @@
 
 int _isalpha(int c)
 {
-return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+{
+
+return (1);
+
+}
+else
+{
+return (0);
+}
+_putchar('\n');
 }
