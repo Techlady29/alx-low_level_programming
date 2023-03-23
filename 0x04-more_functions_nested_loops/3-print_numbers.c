2e7ff8019b474c0e8dd046fb128f68f86487a6c0
Techlady29 <triumphnwabuko29@gmail.com>
2023-03-23 12:49:09 -0700

    main

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,9 +10,11 @@ void print_numbers(void)
 {
 int ch;
 
-for (ch = 48; ch < 58; ch++)
+for (ch = 0; ch <= 9; ch++)
 {
-	_putchar(ch);
+	_putchar(i + '0');
 }
-	_putchar('\n');
+
+_putchar('\n');
+
 }
