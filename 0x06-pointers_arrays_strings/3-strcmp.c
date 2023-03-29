27fe5be7a6f72084696354f04eb9254b7bae976d
Techlady29 <triumphnwabuko29@gmail.com>
2023-03-29 13:58:32 -0700

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,20 +9,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i = 0, op = 0;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	while (op == 0)
 	{
-	if (s1[i] != s2[i])
-	{
-
-	if (s1[i] < s2[i])
-			return (s1[i] - s2[i]);
-	else if (s1[i] > s2[i])
-		return (s1[i] - s2[i]);
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		op = *(s1 + i) - *(s2 + i);
+		i++;
 	}
-	else
-		return (0);
-	}
-	return (0);
+	return (op);
 }
