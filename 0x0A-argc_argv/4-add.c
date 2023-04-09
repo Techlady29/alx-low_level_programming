201d0d586492e5bb93e231822b025da8a7d6b44d
Techlady29 <triumphnwabuko29@gmail.com>
2023-04-09 09:50:21 -0700

    adds

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 
 	for (a = 1; a < argc; a++)
 	{
-		for (a = 0; argv[a][f] != '\0'; f++)
+		for (f = 0; argv[a][f] != '\0'; f++)
 		{
 			if (!isdigit(argv[a][f]))
 			{
