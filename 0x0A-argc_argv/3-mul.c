cc78c2e9d04100182ca1f09712ff5abdb5179c23
Techlady29 <triumphnwabuko29@gmail.com>
2023-04-09 09:21:13 -0700

    multiples

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,11 +10,11 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
+	if (argc != 5)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	printf("%d\n", atoi(argv[2]) * atoi(argv[3]));
 	return (0);
 }
