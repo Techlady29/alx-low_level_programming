f684c346539a8c1ef499f274fddb89c32816cef7
Techlady29 <triumphnwabuko29@gmail.com>
2023-04-14 15:07:16 -0700

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,28 +11,29 @@
 
 void _is_zero(char *argv[])
 {
-	int a, asn1 = 1, asn2 = 1;
+	int i, isn1 = 1, isn2 = 1;
 
-	for (a = 0; argv[1][a]; a++)
-		if (argv[1][a] != '0')
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] != '0')
 		{
-			asn1 = 0;
+			isn1 = 0;
 			break;
 		}
 
-	for (a = 0; argv[2][a]; a++)
-		if (argv[2][a] != '0')
+	for (i = 0; argv[2][i]; i++)
+		if (argv[2][i] != '0')
 		{
-			asn2 = 0;
+			isn2 = 0;
 			break;
 		}
 
-	if (asn1 == 1 || asn2 == 1)
+	if (isn1 == 1 || isn2 == 1)
 	{
 		printf("0\n");
 		exit(0);
 	}
 }
+
 
 /**
  * _initialize_array - this sets memory to zero in a new array
@@ -43,10 +44,10 @@ void _is_zero(char *argv[])
 
 char *_initialize_array(char *ar, int lar)
 {
-	int a = 0;
+	int i = 0;
 
-	for (a = 0; a < lar; a++)
-		ar[a] = '0';
+	for (i = 0; i < lar; i++)
+		ar[i] = '0';
 	ar[lar] = '\0';
 	return (ar);
 }
@@ -61,16 +62,16 @@ char *_initialize_array(char *ar, int lar)
 
 int _checknum(char *argv[], int n)
 {
-	int bn;
+	int ln;
 
-	for (bn = 0; argv[n][bn]; bn++)
-		if (!isdigit(argv[n][bn]))
+	for (ln = 0; argv[n][ln]; ln++)
+		if (!isdigit(argv[n][ln]))
 		{
 			printf("Error\n");
 			exit(98);
 		}
 
-	return (bn);
+	return (ln);
 }
 
 /**
@@ -83,42 +84,42 @@ int _checknum(char *argv[], int n)
 
 int main(int argc, char *argv[])
 {
-	int bn1, bn2, bnout, add, addl, a, h, t, ba;
+	int ln1, ln2, lnout, add, addl, i, j, k, ca;
 	char *nout;
 
 	if (argc != 3)
 		printf("Error\n"), exit(98);
-	bn1 = _checknum(argv, 1), bn2 = _checknum(argv, 2);
-	_is_zero(argv), bnout = bn1 + bn2, nout = malloc(bnout + 1);
+	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
+	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
 	if (nout == NULL)
 		printf("Error\n"), exit(98);
-	nout = _initialize_array(nout, bnout);
-	t = bnout - 1, a = bn1 - 1, h = bn2 - 1, ba = addl = 0;
-	for (; t >= 0; t--, a--)
+	nout = _initialize_array(nout, lnout);
+	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+	for (; k >= 0; k--, i--)
 	{
-		if (a < 0)
+		if (i < 0)
 		{
 			if (addl > 0)
 			{
-				add = (nout[t] - '0') + addl;
+				add = (nout[k] - '0') + addl;
 				if (add > 9)
-					nout[t - 1] = (add / 10) + '0';
-				nout[t] = (add % 10) + '0';
+					nout[k - 1] = (add / 10) + '0';
+				nout[k] = (add % 10) + '0';
 			}
-			a = bn1 - 1, h--, addl = 0, ba++, t = bnout - (1 + ba);
+			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
 		}
-		if (h < 0)
+		if (j < 0)
 		{
 			if (nout[0] != '0')
 				break;
-			bnout--;
-			free(nout), nout = malloc(bnout + 1), nout = _initialize_array(nout, bnout);
-			t = bnout - 1, a = bn1 - 1, h = bn2 - 1, ba = addl = 0;
+			lnout--;
+			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
+			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 		}
-		if (h >= 0)
+		if (j >= 0)
 		{
-			add = ((argv[1][a] - '0') * (argv[2][h] - '0')) + (nout[t] - '0') + addl;
-			addl = add / 10, nout[t] = (add % 10) + '0';
+			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+			addl = add / 10, nout[k] = (add % 10) + '0';
 		}
 	}
 	printf("%s\n", nout);
