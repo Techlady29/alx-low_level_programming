e85df26604089c0970ee301d75b04f1eaad786a8
Techlady29 <triumphnwabuko29@gmail.com>
2023-09-23 04:06:02 -0700

    update

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,13 +1,13 @@
 #include "hash_tables.h"
 
 /**
- *  * key_index - function that generates a distributed index
- *   * according to a given hash
- *    *
- *     * @key: key passed
- *      * @size: size of the hash tables
- *       * Return: key index
- *        */
+ * key_index - function that generates a distributed index
+ * according to a given hash
+ * @key: key passed
+ * @size: size of the hash tables
+ * Return: key index
+ */
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	if (key == NULL || size == 0)
